e6c1caeeff99d02ef14352970a16de9db78feadc
baubau09 <katietran.182@gmail.com>
2022-03-26 19:38:14 +0700

    input script + audio file and send to  firebase

--- components/AudioUploader.js
+++ components/AudioUploader.js
@@ -1,14 +1,17 @@
-import { useState } from 'react';
-import { auth, storage, STATE_CHANGED } from '../lib/firebase';
+import { useState, useContext } from 'react';
+import { auth, storage, STATE_CHANGED, firestore } from '../lib/firebase';
 import Loader from './Loader';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { ProgressBar, ProgressBarProps } from 'react-bootstrap';
+import { serverTimestamp, query, collection, orderBy, getFirestore, setDoc, doc, addDoc } from 'firebase/firestore';
+import { UserContext } from '../lib/context';
+import toast from 'react-hot-toast';
 // Uploads audio to Firebase Storage
-export default function AudioUploader() {
+export default function AudioUploader({script}) {
     const [uploading, setUploading] = useState(false);
     const [progress, setProgress] = useState(0);
     const [downloadURL, setDownloadURL] = useState(null);
-
+    const { username } = useContext(UserContext);
     // Creates a Firebase Upload Task
     const uploadFile = async (e) => {
         // Get the file
@@ -38,51 +41,80 @@ export default function AudioUploader() {
             });
     };
 
-    const submitData = () => {
+    const submitData = async (e) => {
+        e.preventDefault();
+        const uid = auth.currentUser.uid;
+        //const docRef = doc(firestore, 'users', uid, 'speeches', speechID);
+        const collectionRef = collection(getFirestore(), 'users', uid, 'speeches')
+
+        // Tip: give all fields a default value here
+        const data = {
+            uid,
+            username,
+            wordCount: 0,
+            script: script,
+            audioURL: downloadURL,
+            uploadedAt: serverTimestamp(),
+            updatedAt: serverTimestamp(),
+            fillers: 0,
+            fillersDesc: '',
+            fillersPct: 0,
+            pace: 0,
+            paceDesc: '',
+            pronunErr: 0,
+            pronunErrDesc: '',
+            pronunErrPct: 0
+        };
+
+        //await setDoc(docRef, data);
+        const  newDocRef = await addDoc(collectionRef, data)
+        const newDocID = newDocRef.id
+
+        toast.success('Data uploaded!');
 
     }
 
     return (
         <>
             <div className="d-flex justify-content-center mt-4">
-                <Loader show={uploading} width='120' height='120'/>
-                
+                <Loader show={uploading} width='120' height='120' />
+
             </div>
 
             {
                 uploading &&
                 <>
-                    <div style={{width: 300}} className="mt-2">
-                        <ProgressBar striped variant='warning' now={progress} animated label={`${progress}%`}/>
+                    <div style={{ width: 300 }} className="mt-2">
+                        <ProgressBar striped variant='warning' now={progress} animated label={`${progress}%`} />
                     </div>
                 </>
             }
 
-            
-            
+
+
             {/* <h3 className='text-center fw-bold'>{progress}%</h3> */}
-            
+
             {!uploading && (
-                    <>
+                <>
                     <div className='d-flex justify-content-center'>
-                    <label className="btn btn-secondary-light">
+                        <label className="btn btn-secondary-light">
                             🎤 Upload file
                             <input type="file" onChange={uploadFile} accept="audio/wav,audio/mp4,audio/aac,audio/mpeg" />
                         </label>
                     </div>
-                        
-                    </>
-                )}
+
+                </>
+            )}
             {downloadURL &&
                 <>
                     <div className='d-flex justify-content-center mt-3'>
                         <audio controls>
-                            <source src={downloadURL}/>
+                            <source src={downloadURL} />
                         </audio>
                     </div>
 
                     <div className='d-flex justify-content-center mt-5'>
-                        <button className='btn btn-primary-light'>Submit</button>
+                        <button className='btn btn-primary-light' onClick={submitData}>Submit</button>
                     </div>
                 </>}
         </>
--- pages/speak.js
+++ pages/speak.js
@@ -1,9 +1,66 @@
 import { Tab, Tabs, Row, Col, Nav } from "react-bootstrap"
 import AudioUploader from "../components/AudioUploader"
+import { useForm } from "react-hook-form"
+import { useState, useContext } from 'react';
+import { UserContext } from "../lib/context";
+import toast from "react-hot-toast";
 
 export default function SpeakPage() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [script, setScript] = useState('')
+
+    const  onSubmit = async (values) => {
+        setScript(values.script)
+        toast.success('Save successfully!')
+    }
+
+    // const onSubmit = async (values) => {
+    //     const uid = auth.currentUser.uid;
+    //     const collectionRef = collection(getFirestore(), 'users', uid, 'speeches')
+    //     const data = {
+    //         uid,
+    //         username,
+    //         wordCount: 0,
+    //         uploadedAt: serverTimestamp(),
+    //         updatedAt: serverTimestamp(),
+    //         script: values.script
+    //     }
+    //     const docRef = await addDoc(collectionRef, data);
+
+    //     setSpeechID(docRef.id)
+    //     setInitData(data)
+    //     console.log(docRef.id)
+
+    //     toast.success('Save successfully!')
+    // }
     return (
         <>
+
+            <div className="d-flex justify-content-center">
+            <h3>1. Enter your script here</h3>
+            </div>
+
+            <div className="d-flex justify-content-center">
+            <form onSubmit={handleSubmit(onSubmit)} style={{ width: 600 }}>
+                            <label>
+                                <h6 className="fw-500">What do you want to practice? <span className="text-danger">*</span></h6>
+                            </label>
+                            <textarea className="form-control" {...register("script", { required: "This field is required!", maxLength: { value: 1200, message: "Maximum 1200 characters" } })} rows="6" style={{ boxShadow: "none" }} />
+                            {errors.script && (
+                                <p className="form-error-mess">
+                                    <i className="fas fa-exclamation-circle"></i>&nbsp;{errors.script.message}
+                                </p>
+                            )}
+                            <button type="submit" className="mt-5 btn btn-primary">
+                                Save script
+                            </button>
+                        </form>
+            
+            </div>
+
+            <div className="d-flex justify-content-center mt-5">
+            <h3>2. Record or Upload your speech</h3>
+            </div>
             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
                 <div className="d-flex justify-content-center">
                     <Nav variant="pills" className="btn-lg">
@@ -24,7 +81,7 @@ export default function SpeakPage() {
                             Record
                         </Tab.Pane>
                         <Tab.Pane eventKey="second">
-                            <AudioUploader/>
+                            <AudioUploader script={script}/>
                         </Tab.Pane>
                     </Tab.Content>
                 </div>
